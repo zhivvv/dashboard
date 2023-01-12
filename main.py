83c2b8e085ca7575c473fdfb282386d2f9f6fecf
ivanov.ev <ivanov.eune@gmail.com>
2023-01-13 02:36:07 +0300

    StratModel.py deleted, folder with package deleted

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from gui import GUI
 import sys
 from PyQt5 import QtWidgets
 
-
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     ui = GUI()
     ui.show()
     sys.exit(app.exec_())
-
